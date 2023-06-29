4ed628994621cabdc3bcd1188f1d5eefa4720806
Tunnes <cauepani06@gmail.com>
2023-06-28 22:43:30 -0300

    Define theme to StyledComponents, Create GlobalStyle and Define new font-family to the app

---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+import { defaultTheme } from './theme/default'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${defaultTheme.colors.blue[500]};
+  }
+
+  html, body {
+    width: 100%;
+    background: ${defaultTheme.colors.white};
+    color: ${defaultTheme.colors.blue[800]};
+    min-height: 100vh;
+  }
+
+  body, input, textarea, button {
+    font-family: 'JakartaRegular', sans-serif;
+    font-weight: 400;
+    color: ${defaultTheme.colors.blue[800]};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'JakartaBold', sans-serif;
+    font-weight: 700;
+  }
+
+  @media screen and (max-width: 1120px) {
+    html {
+      font-size: 93.75% !important;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    html {
+      font-size: 87.5% !important;
+    }
+  }
+`
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    blue: {
+      800: '#1c3c50',
+      700: '#3A5667',
+      550: '#007eff',
+      500: '#0983ff',
+      450: '#2c83fb',
+      400: '#3599ff',
+      300: '#6DB5FF',
+      200: '#e6f3ff',
+      100: '#f0f8ff',
+    },
+    gray: {
+      // 700: '#506777',
+      500: '#8C98A2',
+      300: '#C0D0D5',
+      200: '#cbd6df',
+    },
+  },
+} as const
