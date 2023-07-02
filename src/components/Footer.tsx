869bfbb908c5bd47a81aac5577a44c88c16f4c0a
Tunnes <cauepani06@gmail.com>
2023-07-01 21:06:18 -0300

    fix: Styles Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import LogoIcon from '@/assets/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import { LuFacebook, LuInstagram, LuLinkedin } from 'react-icons/lu'
 export const FooterContainer = styled.footer`
   width: 100%;
   padding: 2rem;
@@ -52,17 +53,6 @@ const Section = styled.section`
 
     margin-bottom: 1rem;
   }
-
-  p {
-    font-size: 1rem;
-    font-family: 'JakartaRegular';
-    color: ${(props) => props.theme.colors.gray[300]};
-    cursor: pointer;
-    &:hover {
-      color: ${(props) => props.theme.colors.blue[800]};
-      transition: color 0.4s;
-    }
-  }
 `
 
 const BoxCopyright = styled.section`
@@ -73,13 +63,14 @@ const BoxCopyright = styled.section`
   flex-wrap: wrap;
   gap: 0.5rem;
 
+  margin-top: 3rem;
   @media screen and (max-width: 1024px) {
     justify-content: center;
   }
 
   h5 {
     font-size: 0.75rem;
-    color: ${(props) => props.theme.colors.blue[800]};
+    color: ${(props) => props.theme.colors.gray[500]};
   }
   h6 {
     font-size: 0.75rem;
@@ -92,6 +83,64 @@ const BoxCopyright = styled.section`
   }
 `
 
+const FlexCenter = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
+
+const SocialMediaButton = styled(Link)`
+  background: ${(props) => props.theme.colors.gray[300]};
+  cursor: pointer;
+
+  > svg {
+    color: ${(props) => props.theme.colors.gray[500]};
+  }
+
+  &:hover {
+    > svg {
+      transform: scale(1.2);
+      transition: all 0.4s;
+      color: ${(props) => props.theme.colors.white};
+    }
+
+    background: ${(props) => props.theme.colors.blue[500]};
+  }
+
+  transition: all 0.4s ease-in-out;
+  font-size: 1.25rem;
+  padding: 0.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: none;
+  border-radius: 50%;
+`
+interface TextProps {
+  notIsLink?: boolean
+}
+
+const Text = styled.p<TextProps>`
+  font-size: 0.875rem;
+  font-family: 'JakartaRegular';
+  color: ${(props) => props.theme.colors.gray[500]};
+  cursor: pointer;
+
+  > b {
+    color: ${(props) => props.theme.colors.blue[800]};
+  }
+
+  ${(props) =>
+    !props.notIsLink &&
+    css`
+      &:hover {
+        color: ${(props) => props.theme.colors.blue[800]};
+        transition: color 0.4s;
+      }
+    `}
+`
+
 export function Footer() {
   return (
     <FooterContainer>
@@ -101,24 +150,46 @@ export function Footer() {
         <GroupSection>
           <Section>
             <h3>Links Principais</h3>
-            <p>Home</p>
-            <p>Ferramenta</p>
-            <p>Preços</p>
-            <p>Contato</p>
+            <Text>Home</Text>
+            <Text>Ferramenta</Text>
+            <Text>Preços</Text>
+            <Text>Contato</Text>
           </Section>
           <Section>
             <h3>Cases</h3>
-            <p>Home</p>
-            <p>Ferramenta</p>
-            <p>Preços</p>
-            <p>Contato</p>
+            <Text>Home</Text>
+            <Text>Ferramenta</Text>
+            <Text>Preços</Text>
+            <Text>Contato</Text>
           </Section>
           <Section>
             <h3>Materiais</h3>
-            <p>Home</p>
-            <p>Ferramenta</p>
-            <p>Preços</p>
-            <p>Contato</p>
+            <Text>Home</Text>
+            <Text>Ferramenta</Text>
+            <Text>Preços</Text>
+            <Text>Contato</Text>
+          </Section>
+
+          <Section>
+            <h3>Siga a Leadster</h3>
+            <FlexCenter>
+              <SocialMediaButton href="https://www.linkedin.com/company/getleadster/">
+                <LuLinkedin fontWeight={'bold'} />
+              </SocialMediaButton>
+              <SocialMediaButton href="https://www.facebook.com/leadsterplatform">
+                <LuFacebook />
+              </SocialMediaButton>
+              <SocialMediaButton href="https://www.instagram.com/leadster.com.br/">
+                <LuInstagram />
+              </SocialMediaButton>
+            </FlexCenter>
+
+            <Text notIsLink>
+              <b>E-mail:</b> contato@leadster.com.br
+            </Text>
+            <Text notIsLink>
+              <b>Telefone:</b> (42) 98828-9851
+            </Text>
           </Section>
         </GroupSection>
 
